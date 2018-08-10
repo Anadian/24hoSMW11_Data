025ee52102ed05132695bea21e53facff5ac2312
Anadian <willanad@yandex.com>
2018-08-10 01:00:01 -0400

    Cron.sh auto commit.

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const FileSystem = require('fs');
 //
 var file_data = FileSystem.readFileSync('24ho11-2.json', 'utf8');
 var json_object = JSON.parse(file_data);
+var deathless_stages = 0;
+var most_deaths = null;
+var total_deaths = 0;
+var deathless_score = 0;
+var deaths_score = 0;
 var title_screen_true = [];
 var title_screen_false = [];
 var player_sprite_true = [];
@@ -36,91 +41,102 @@ var yoshi_true = [];
 var yoshi_false = [];
 var auto_scroller_true = [];
 var auto_scroller_false = [];
+var report = '';
 for(var i = 0; i < json_object.entries.length; i++){
+	if( json_object.entries[i].video.deaths.length == 0 ){
+		deathless_stages++;
+		deathless_score.push(json_object.entries[i].score.total);
+	} else{
+		total_deaths += json_object.entries[i].video.deaths.length;
+		if( json_object.entries[i].video.deaths.length > most_deaths ){
+			most_deaths = i;
+		} 
+	
 	//title screen X/33: mean avg-yes | avg-no
-	if( json_object.entries[i].custom.title_screen === true ){
+	if( json_object.entries[i].video.custom.title_screen === true ){
 		title_screen_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].custom.title_screen === false ){
+	} else if( json_object.entries[i].video.custom.title_screen === false ){
 		title_screen_false.push(json_object.entries[i].score);
 	}
 	//player sprite
-	if( json_object.entries[i].custom.player_sprite === true ){
+	if( json_object.entries[i].video.custom.player_sprite === true ){
 		player_sprite_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].custom.player_sprite === false ){
+	} else if( json_object.entries[i].video.custom.player_sprite === false ){
 		player_sprite_false.push(json_object.entries[i].score);
 	}
 	//hud
-	if( json_object.entries[i].custom.hud === true ){
+	if( json_object.entries[i].video.custom.hud === true ){
 		hud_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].custom.hud === false ){
+	} else if( json_object.entries[i].video.custom.hud === false ){
 		hud_false.push(json_object.entries[i].score);
 	}
 	//overworld
-	if( json_object.entries[i].custom.overworld === true ){
+	if( json_object.entries[i].video.custom.overworld === true ){
 		overworld_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].custom.overworld === false ){
+	} else if( json_object.entries[i].video.custom.overworld === false ){
 		overworld_false.push(json_object.entries[i].score);
 	}
 	//dragon coins
-	if( json_object.entries[i].features.dragon_coins === true ){
+	if( json_object.entries[i].video.features.dragon_coins === true ){
 		dragon_coins_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.dragon_coins === false ){
+	} else if( json_object.entries[i].video.features.dragon_coins === false ){
 		dragon_coins_false.push(json_object.entries[i].score);
 	}
 	//puzzles
-	if( json_object.entries[i].features.puzzles === true ){
+	if( json_object.entries[i].video.features.puzzles === true ){
 		puzzles_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.puzzles === false ){
+	} else if( json_object.entries[i].video.features.puzzles === false ){
 		puzzles_false.push(json_object.entries[i].score);
 	}
 	//boss fights
-	if( json_object.entries[i].features.boss_fights === true ){
+	if( json_object.entries[i].video.features.boss_fights === true ){
 		boss_fights_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.boss_fights === false ){
+	} else if( json_object.entries[i].video.features.boss_fights === false ){
 		boss_fights_false.push(json_object.entries[i].score);
 	}
 	//text
-	if( json_object.entries[i].features.text === true ){
+	if( json_object.entries[i].video.features.text === true ){
 		text_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.text === false ){
+	} else if( json_object.entries[i].video.features.text === false ){
 		text_false.push(json_object.entries[i].score);
 	}
 	//quick restart
-	if( json_object.entries[i].features.quick_restart === true ){
+	if( json_object.entries[i].video.features.quick_restart === true ){
 		quick_restart_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.quick_restart === false ){
+	} else if( json_object.entries[i].video.features.quick_restart === false ){
 		quick_restart_false.push(json_object.entries[i].score);
 	}
 	//life farm
-	if( json_object.entries[i].features.life_farm === true ){
+	if( json_object.entries[i].video.features.life_farm === true ){
 		life_farm_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.life_farm === false ){
+	} else if( json_object.entries[i].video.features.life_farm === false ){
 		life_farm_false.push(json_object.entries[i].score);
 	}
 	//secret exit
-	if( json_object.entries[i].features.secret_exit === true ){
+	if( json_object.entries[i].video.features.secret_exit === true ){
 		secret_exit_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.secret_exit === false ){
+	} else if( json_object.entries[i].video.features.secret_exit === false ){
 		secret_exit_false.push(json_object.entries[i].score);
 	}
 	//memes
-	if( json_object.entries[i].features.memes === true ){
+	if( json_object.entries[i].video.features.memes === true ){
 		memes_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.memes === false ){
+	} else if( json_object.entries[i].video.features.memes === false ){
 		memes_false.push(json_object.entries[i].score);
 	}
 	//yoshi
-	if( json_object.entries[i].features.yoshi === true ){
+	if( json_object.entries[i].video.features.yoshi === true ){
 		yoshi_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.yoshi === false ){
+	} else if( json_object.entries[i].video.features.yoshi === false ){
 		yoshi_false.push(json_object.entries[i].score);
 	}
 	//auto_scroller
-	if( json_object.entries[i].features.auto_scroller === true ){
+	if( json_object.entries[i].video.features.auto_scroller === true ){
 		auto_scroller_true.push(json_object.entries[i].score);
-	} else if( json_object.entries[i].features.auto_scroller === false ){
+	} else if( json_object.entries[i].video.features.auto_scroller === false ){
 		auto_scroller_false.push(json_object.entries[i].score);
 	}
+
 }
 file_data = JSON.stringify(json_object, null, '\t');
 FileSystem.writeFileSync('24ho11-2.json', file_data, 'utf8');
